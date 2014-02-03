6e054006372828365263d651e1991190ccc72acc
sethvincent <sethvincent@gmail.com>
2014-02-03 14:34:16 -0800

    use new bin/dist system

--- index.js
+++
@@ -1,9 +0,0 @@
-var ReadmeBuilder = require('manuscript-builder');
-
-var readme = new ReadmeBuilder({
-  target: '/book.md',
-  bookDir: '/manuscript/',
-  tableOfContents: '/manuscript/Book.txt'
-});
-
-readme.build();
